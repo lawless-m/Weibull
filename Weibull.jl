cfde44531b43139546968e755c24c13ac5218aee
Matt H <lawless-m@users.noreply.github.com>
2019-05-24 21:40:18 +0100

    tidied up and chaged attribution

--- Weibull.jl
+++ Weibull.jl
@@ -1,10 +1,8 @@
 #==
 
-https://stats.stackexchange.com/questions/19866/how-to-fit-a-weibull-distribution-to-input-data-containing-zeroes
+uses JuMP instead of Goal seeker and Solver as described in
 
-
-implements the Blischke-Scheuer method-of-moments estimation of (a,b)
-for the Weibull distribution F(t) = 1 - exp(-(t/a)^b)
+https://www.real-statistics.com/distribution-fitting/distribution-fitting-via-maximum-likelihood/fitting-weibull-parameters-mle/
 
 ==#
 
@@ -12,13 +10,11 @@ using Roots # Add.pkg("Roots")
 using JuMP
 using Ipopt
 
-gln(n) = log(gamma(n))
-
 function simplefit(xbar, svar)
     model = Model(solver=IpoptSolver())
     @variable(model, n, start = 1)
     @constraint(model, n >= 1)
-    @NLobjective(model, Min, (log(gamma(1+2/n)) - 2log(gamma(1+1/n)) - log(xbar*xbar+svar*svar) + 2log(xbar))^2)
+    @NLobjective(model, Min, (log(gamma(1+2/n)) - 2log(gamma(1+1/n)) - log(xbar^2+svar^2) + 2log(xbar))^2)
     TT = STDOUT # save original STDOUT stream
     redirect_stdout()
     solve(model)
@@ -50,9 +46,3 @@ function fit(samples)
 
     alpha, beta
 end
-
-samples = [23,19,37,38,40,36,172,48,113,90,54,104,90,54,157,51,77,78,144,34,29,45,16,15,37,218,170,44,121]
-samples = [509, 660, 386, 753, 811, 613, 848, 725, 315, 872, 487, 512]
-a, b = fit(samples)
-
-println("a $a, b $b")
